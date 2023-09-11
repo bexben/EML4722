13c8e6a5cd91e8fb14e6691f3be5256cc1fff186
benjamin jones <bexben9@gmail.com>
2023-09-10 20:55:28 -0400

    q1 work

--- homework/hw1/Q1_Files/Q1.py
+++ homework/hw1/Q1_Files/Q1.py
@@ -3,27 +3,28 @@ import os
 import subprocess
 import pandas as pd
 
-history_filename = '/home/bexben/school/CFD/homework/hw1/Q1_Files/history.csv'
+history_filename = '/home/bexben/school/CFD/homework/hw1/Q1_Files/iterations/history.csv'
 cfg_filename = '/home/bexben/school/CFD/homework/hw1/Q1_Files/inv_NACA0012.cfg'
 cfg_child_filename = '/home/bexben/school/CFD/homework/hw1/Q1_Files/inv_NACA0012_child.cfg'
+flow_filename = '/home/bexben/school/CFD/homework/hw1/Q1_Files/iterations/flow'
 
 variable = 'AOA' # variable to be edited
 var_range = np.linspace(0, 15, 16) # values to test
 
 
-def edit_line(variable, var_value) -> int:
-    varlen = len(variable)
-    num_edits = 0
-
+def edit_line(variable_list, var_value_list) -> int:
     cfg_file = open(cfg_filename, 'r')
     cfg_list = cfg_file.readlines()
     cfg_file.close()
-    # os.remove(cfg)
+    num_edits = 0
 
-    for index, element in enumerate(cfg_list):
-        if element[:varlen] == variable:
-            cfg_list[index] = variable + '=' + str(var_value)
-            num_edits += 1
+    for varIndex, (variable, var_value) in enumerate(zip(variable_list, var_value_list)):
+        varlen = len(variable)
+
+        for index, element in enumerate(cfg_list):
+            if element[:varlen] == variable:
+                cfg_list[index] = variable + '=' + str(var_value)
+                num_edits += 1
 
     with open(cfg_child_filename, 'w') as new_cfg:
         for line in cfg_list:
@@ -41,8 +42,14 @@ def log_output(variable, var_value):
 def run_tests(variable, var_range):
     for index, var_value in enumerate(var_range):
         history_path_edited = history_filename[:-4] + '_' + variable + str(var_value)
-        edit_line(variable, var_value) # First one must set the variable
+        flow_filename_edited = flow_filename + '_' + variable + str(var_value)
+        '''edit_line(variable, var_value) # First one must set the variable
         edit_line('CONV_FILENAME', history_path_edited)
+        edit_line('VOLUME_FILENAME', flow_filename_edited)'''
+
+        edits = edit_line([variable, 'CONV_FILENAME', 'VOLUME_FILENAME'], [var_value, history_path_edited, flow_filename_edited])
+
+        print(edits)
 
         # now we must run the file
         output = subprocess.run(['SU2_CFD', cfg_child_filename], check=True, text=True)
